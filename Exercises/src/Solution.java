3666b6ccf514eef65dd1923623fdde3ce61a8606
TJG <jarag@10.100.228.238>
2017-09-21 09:42:49 -0400

    First commit in Eclipse

---
+++ Exercises/src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
+		
+		// Read a full line of input from stdin and save it to our variable, inputString.
+		String inputString = scan.nextLine(); 
+
+		// Close the scanner object, because we've finished reading 
+        // all of the input from stdin needed for this challenge.
+		scan.close(); 
+      
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello, World.");
+      
+	    // Write a line of code here that prints the contents of inputString to stdout.
+        System.out.println(inputString);
+	}
+}
+
+public class TG {
+	// A class is code
+	/**
+	 * Tyrese Galloway
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// int gear;  // declaration
+		// int gear = 1 //
+		// gear = 5;  // assignment
+		
+		Scanner keyboard = new Scanner(System.in); 
+			// Single line comment
+			System.out.println("Welcome to My Integration Project");
+	}
+}
+
